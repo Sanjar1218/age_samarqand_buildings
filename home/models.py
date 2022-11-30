f5c71b56e7a1acc3cffaae2a4854e6116b2b6e5e
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-30 13:35:04 +0500

    adding to admin

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    address = models.CharField(max_length=255)
+    type_of_building = models.CharField(max_length=255)
+    count_floor = models.IntegerField(blank=True)
+    date = models.DateField()
+    center = models.JSONField()
+    coordinates = models.JSONField()
+
+    def __str__(self):
+        return self.type_of_building
