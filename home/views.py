2592d404fb7fcdfc139ceba44038b45779c8161b
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-30 13:50:39 +0500

    create home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import BuildingSerializer
+
+
+@api_view(['POST'])
+def create_home(request: Request) -> Response:
+    serializer = BuildingSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'status': 'created'})
+    return Response({'status': 'bad', 'errros': serializer.errors})
+
+
+@api_view(['GET'])
+def get_home(request: Request) -> Response:
+    pass
