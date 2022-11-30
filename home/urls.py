26e1aa7993e02b3623feaa4a169ad4cb4d490edf
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-30 14:22:14 +0500

    minor fix

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
     # CRUD
     path('create_home', views.create_home),
-    path('get_home', views.get_home),
-    path('update_home', views.update_home),
+    path('get_homes', views.get_homes),
+    path('update_home/<int:id>', views.update_home),
     path('delete_home', views.delete_home),
 ]
