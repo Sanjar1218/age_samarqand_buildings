a8e547ec0f1defe32d4e3ee088c5bc9a2ba98969
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-30 14:22:45 +0500

    cred comleted

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import api_view
 
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
 from .serializers import BuildingSerializer
+from .models import Building
 
 
 @api_view(['POST'])
@@ -15,5 +18,31 @@ def create_home(request: Request) -> Response:
 
 
 @api_view(['GET'])
-def get_home(request: Request) -> Response:
-    pass
+def get_homes(request: Request) -> Response:
+    all_buldings = Building.objects.all()
+
+    serializer = BuildingSerializer(all_buldings, many=True)
+
+    return Response({'buldings': serializer.data})
+
+
+@api_view(['POST'])
+def update_home(request: Request, id) -> Response:
+    building = Building.objects.get(id=id)
+    serializer = BuildingSerializer(building, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'status': 'updated'})
+    return Response({'status': serializer.errors})
+
+
+@api_view(['POST'])
+def delete_home(request: Request, id) -> Response:
+    try:
+        building = Building.objects.get(id=id)
+        building.delete()
+        return Response({'status': 'deleted'})
+    except ObjectDoesNotExist:
+        return Response({'status': 'does not exists bulding'})
+    except MultipleObjectsReturned:
+        return Response({'status': 'multiple objects returned'})
