bb165d102e3c34affeeb6130b506734516431ff6
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-11-30 14:35:14 +0500

    minor fix

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('create_home', views.create_home),
     path('get_homes', views.get_homes),
     path('update_home/<int:id>', views.update_home),
-    path('delete_home', views.delete_home),
+    path('delete_home/<int:id>', views.delete_home),
 ]
