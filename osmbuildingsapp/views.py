384fa188c8b3ca2a570c836362944a03fb267782
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-12-03 13:03:36 +0500

    two function

--- osmbuildingsapp/views.py
+++ osmbuildingsapp/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
-def home(requests: HttpRequest) -> HttpResponse:
+def osmbuilding(requests: HttpRequest) -> HttpResponse:
     return render(requests, 'home.html')
+
+
+def leaflet(requests: HttpRequest) -> HttpResponse:
+    return render(requests, 'leaflet.html')
