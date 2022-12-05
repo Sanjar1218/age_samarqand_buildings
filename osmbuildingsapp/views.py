4dd49f83d5dbb59069f2e14f77ab5d88a3c6b446
Sanjar1218 <sanjarsaidov173@gmail.com>
2022-12-05 15:55:34 +0500

    directly getting date from database

--- osmbuildingsapp/views.py
+++ osmbuildingsapp/views.py
@@ -4,19 +4,23 @@ from django.http import HttpRequest, HttpResponse
 from home.models import Building
 from home.serializers import BuildingSerializer
 
+import json
+
 
 def osmbuilding(requests: HttpRequest) -> HttpResponse:
-    return render(requests, 'home.html')
+    return render(requests, "home.html")
 
 
 def leaflet(requests: HttpRequest) -> HttpResponse:
-    return render(requests, 'leaflet.html')
+    return render(requests, "leaflet.html")
 
 
 def django_leaflet(requests: HttpRequest) -> HttpResponse:
     all_buldings = Building.objects.all()
-
+    lst = []
     serializer = BuildingSerializer(all_buldings, many=True)
+    for i in serializer.data:
+        lst.append({"type": "Feature", "geometry": {
+                   "type": "Polygon", "coordinates": i["coordinates"]}})
 
-    # print(serializer.data)
-    return render(requests, 'django_leaflet.html', context={'data': serializer.data})
+    return render(requests, "django_leaflet.html", context={"data": json.dumps(lst)})
